fdc801c1e2ae6b490acf35849f5bb6d9b9df071a
Benoit Louy <benoit.louy@fastmail.com>
2021-12-03 23:17:43 -0500

    add timeout option to batcher

--- tele/src/test/scala/tele/BatcherSpec.scala
+++ tele/src/test/scala/tele/BatcherSpec.scala
@@ -1,8 +1,11 @@
 package tele
 
-import cats.data.NonEmptyVector
+import scala.concurrent.duration._
 
-class BatcherSpec extends munit.FunSuite {
+import cats.data.NonEmptyVector
+import cats.effect._
+
+class BatcherSpec extends munit.CatsEffectSuite {
 
   implicit val stringSchema: Schema[String] = new Schema[String] {
     override def encode(a: String): Array[Byte] = a.getBytes()
@@ -14,45 +17,69 @@ class BatcherSpec extends munit.FunSuite {
   test("batch size limit reached") {
     val result = fs2
       .Stream("data1", "data2", "data3", "data4")
+      .covary[IO]
       .through(Batcher.batch(Batcher.Options[String]().withPartitionKey(_ => "a").withMaxBatchSize(15)))
       .compile
       .toVector
-      .collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) }
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
 
-    assertEquals(result, Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
   }
 
   test("batch across chunks") {
     val result = (fs2.Stream("data1") ++ fs2.Stream("data2") ++ fs2.Stream("data3", "data4"))
+      .covary[IO]
       .through(Batcher.batch(Batcher.Options[String]().withPartitionKey(_ => "a").withMaxBatchSize(15)))
       .compile
       .toVector
-      .collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) }
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
 
-    assertEquals(result, Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
   }
 
   test("batch count limit reached") {
     val result = fs2
       .Stream("data1", "data2", "data3", "data4")
+      .covary[IO]
       .through(Batcher.batch(Batcher.Options[String]().withPartitionKey(_ => "a").withMaxEntryCount(3)))
       .compile
       .toVector
-      .collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) }
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
 
-    assertEquals(result, Vector(NonEmptyVector.of("data1", "data2", "data3"), NonEmptyVector.one("data4")))
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2", "data3"), NonEmptyVector.one("data4")))
   }
 
   test("exclude large data") {
     val result = fs2
       .Stream("data1", "data2", "tooLarge", "data3", "data4")
+      .covary[IO]
       .through(
         Batcher.batch(Batcher.Options[String]().withPartitionKey(_ => "a").withMaxBatchSize(15).withMaxEntrySize(6))
       )
       .compile
       .toVector
-      .collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) }
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
 
-    assertEquals(result, Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
+  }
+
+  test("no timeout") {
+    val result = (fs2.Stream("data1", "data2") ++ fs2.Stream.sleep_[IO](200.millis) ++ fs2.Stream("data3", "data4"))
+      .through(Batcher.batch(Batcher.Options[String]()))
+      .compile
+      .toVector
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
+
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2", "data3", "data4")))
+  }
+
+  test("timeout") {
+    val result = (fs2.Stream("data1", "data2") ++ fs2.Stream.sleep_[IO](200.millis) ++ fs2.Stream("data3", "data4"))
+      .through(Batcher.batch(Batcher.Options[String]().withTimeout(150.millis)))
+      .compile
+      .toVector
+      .map(_.collect { case Batcher.Batch(buf, _, _) => buf.map(_.value) })
+
+    result.assertEquals(Vector(NonEmptyVector.of("data1", "data2"), NonEmptyVector.of("data3", "data4")))
   }
 }
